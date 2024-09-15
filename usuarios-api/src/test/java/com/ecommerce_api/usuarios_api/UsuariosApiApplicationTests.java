e3953148a39f761f6473a704bf7096227939d55d
Walisson oliveira <walissonoliveira1@outlook.com>
2024-09-15 20:53:17 -0300

    Inicio do projeto

---
+++ usuarios-api/src/test/java/com/ecommerce_api/usuarios_api/UsuariosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce_api.usuarios_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
