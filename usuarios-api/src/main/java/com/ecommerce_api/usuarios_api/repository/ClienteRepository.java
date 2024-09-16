8aebeb89083cd5863917efcad575fc9aa17d4ceb
Walisson oliveira <walissonoliveira1@outlook.com>
2024-09-15 22:31:50 -0300

    Criação de pasta repository/arquivo: ClienteRepository

---
+++ usuarios-api/src/main/java/com/ecommerce_api/usuarios_api/repository/ClienteRepository.java
@@ -0,0 +1,25 @@
+package com.ecommerce_api.usuarios_api.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ecommerce_api.usuarios_api.models.Cliente;
+import java.util.List;
+import java.time.LocalDate;
+
+
+@Repository
+public interface ClienteRepository  extends JpaRepository<Cliente, Long> {
+
+    Optional<Cliente> findByCpf(String cpf);
+
+    List<Cliente> findByDataNascimentoBetween(LocalDate dataInicial, LocalDate dataFinal);
+
+    Optional<Cliente> findByUsuarios_Email(String email);
+
+    List<Cliente> findByUsuario_NomeContainsIgnoreCase(String nome);
+
+    List<Cliente> findByDataNascimento(LocalDate dataNascimento);
+    
+}
