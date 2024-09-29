80918de072f1bd6fc5171e77d6fa89780ea588c9
Walisson oliveira <walissonoliveira1@outlook.com>
2024-09-29 12:37:10 -0300

    Ajustes de criação de tabelas com fly migration

--- usuarios-api/src/main/resources/db/migration/V2__INSERT_USUARIO_ADMIN.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `eco_usuarios`.`tb_usuarios` 
-(`id`, `administrador`, `celular`, `colaborador`, `email`, `nome`, `senha`, `telefone`, `usuario_externo`) 
-VALUES 
-('1', 1, '(81) 99160 3025', 0, 'alexandre@treinarecife.com.br', 'Alexandre de Souza Junior', '$2a$12$NRE4iVnAaBr12BQEJEQbA.841xKzBbz36ct2daX9Alk51eyKc17ny', '', 0);
--- usuarios-api/src/main/resources/db/migration/V3__CREATE_TABLE_CLIENTES_FORNECEDORES.sql
+++ usuarios-api/src/main/resources/db/migration/V3__CREATE_TABLE_CLIENTES_FORNECEDORES.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `tb_clientes`(
     `id` BIGINT PRIMARY KEY AUTO_INCREMENT,
     `cpf` VARCHAR(11) NOT NULL UNIQUE,
-    `data_nascimento` DATA NOT NULL,
+    `data_nascimento` DATE NOT NULL,
     `usuario_id` BIGINT NOT NULL,
     FOREIGN KEY (`usuario_id`) REFERENCES `tb_usuarios`(id)
 );
