531b04d728e4926c745bdd35ce163f412e525092
Walisson oliveira <walissonoliveira1@outlook.com>
2024-10-01 18:32:04 -0300

    Criação das tabelas fornecedor e cliente

--- usuarios-api/src/main/resources/db/migration/V3__CREATE_TABLE_CLIENTES_FORNECEDORES.sql
+++ usuarios-api/src/main/resources/db/migration/V3__CREATE_TABLE_CLIENTES_FORNECEDORES.sql
@@ -5,3 +5,11 @@ CREATE TABLE IF NOT EXISTS `tb_clientes`(
     `usuario_id` BIGINT NOT NULL,
     FOREIGN KEY (`usuario_id`) REFERENCES `tb_usuarios`(id)
 );
+CREATE TABLE IF NOT EXISTS `tb_fornecedores`(
+    `id` BIGINT PRIMARY KEY AUTO_INCREMENT,
+    `cnpj` VARCHAR(14) NOT NULL UNIQUE,
+    `razao_social` VARCHAR(255) NOT NULL UNIQUE,
+    `usuario_id` BIGINT NOT NULL,
+    FOREIGN KEY(`usuario_id`) REFERENCES `tb_usuarios`(`id`)
+)
+
