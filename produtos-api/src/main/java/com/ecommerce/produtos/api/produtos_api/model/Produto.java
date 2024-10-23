d16071636aee8e3853e1c2eb0af0d6e822889fe5
Walisson oliveira <walissonoliveira1@outlook.com>
2024-10-22 22:34:04 -0300

    Fiz uma alteração importando novamente o projeto produtos-api e fiz a classe produto

---
+++ produtos-api/src/main/java/com/ecommerce/produtos/api/produtos_api/model/Produto.java
@@ -0,0 +1,39 @@
+package com.ecommerce.produtos.api.produtos_api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String codigo;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String descricao;
+
+    @Column(nullable = false)
+    private Double precoDouble;
+
+
+
+
+    
+    
+}
