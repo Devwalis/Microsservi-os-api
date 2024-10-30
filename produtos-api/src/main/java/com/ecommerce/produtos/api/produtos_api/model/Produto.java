961ccffa7ef70f37a9fc4e0cabaa41ee3b621363
Walisson oliveira <walissonoliveira1@outlook.com>
2024-10-29 22:27:11 -0300

    produtos-api conexão com o banco ok e alterações de relacionamento de tabelas

--- produtos-api/src/main/java/com/ecommerce/produtos/api/produtos_api/model/Produto.java
+++ produtos-api/src/main/java/com/ecommerce/produtos/api/produtos_api/model/Produto.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -30,6 +32,10 @@ public class Produto {
 
     @Column(nullable = false)
     private Double precoDouble;
+
+    @ManyToOne
+    @JoinColumn(name ="categoria_id", nullable = false)
+    private Categoria categoria;
 
 
 
---
+++ produtos-api/src/main/java/com/ecommerce/produtos/api/produtos_api/service/AutenticacaoService.java
@@ -0,0 +1,7 @@
+package com.ecommerce.produtos.api.produtos_api.service;
+
+public class AutenticacaoService {
+
+    // TODO TODO
+    
+}
