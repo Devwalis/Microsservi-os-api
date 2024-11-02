0fb0ab92abf57910092a7b0c1d8c624baf7b3ded
Walisson oliveira <walissonoliveira1@outlook.com>
2024-11-02 10:12:33 -0300

    Web-client para armazenar as entidades dtos

---
+++ web-client/src/main/java/main/java/com/ecommerce/web/client/web_client/usuario/UsuarioDTO.java
@@ -0,0 +1,23 @@
+package main.java.com.ecommerce.web.client.web_client.usuario;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioDTO {
+    private String nome;
+    private String email;
+    private String telefone;
+    private String celular;
+    private Boolean administrador;
+    private Boolean colaborador;
+    private Boolean usuarioExterno;
+    private String token;
+
+
+
+    
+}
